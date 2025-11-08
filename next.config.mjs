8cc571c6e867087a2ba74b7d92a2db836912a69e
Kenneth Yap <85454120+kenneth-yap@users.noreply.github.com>
2025-11-08 14:53:04 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // For static export to GitHub Pages
   images: {
     unoptimized: true,  // Required for static export
   },
